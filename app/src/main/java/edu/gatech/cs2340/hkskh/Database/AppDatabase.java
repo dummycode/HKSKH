fc615b118f53b9b87cae4438b245dd5c051b8208
Henry Harris <henrythomasharris@gmail.com>
2018-03-15 13:32:07 -0400

    Actual persistence, whoops :P

--- app/src/main/java/edu/gatech/cs2340/hkskh/Database/AppDatabase.java
+++ app/src/main/java/edu/gatech/cs2340/hkskh/Database/AppDatabase.java
@@ -26,7 +26,7 @@ public abstract class AppDatabase extends RoomDatabase {
     public static AppDatabase getInMemoryDatabase(Context context) {
         if (INSTANCE == null) {
             INSTANCE =
-                    Room.inMemoryDatabaseBuilder(context.getApplicationContext(), AppDatabase.class)
+                    Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "hkskh")
                             // To simplify the codelab, allow queries on the main thread.
                             // Don't do this on a real app! See PersistenceBasicSample for an example.
                             .allowMainThreadQueries()
