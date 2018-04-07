8079d1f63ebd0d4b4248637c0f276bfe47c54847
kirbykasischke192 <krkazz@comcast.net>
2018-04-07 11:10:08 -0400

    Added ResgisterUserTest

---
+++ app/src/test/java/edu/gatech/cs2340/hkskh/RegisterUserTest.java
@@ -0,0 +1,78 @@
+package edu.gatech.cs2340.hkskh;
+
+import android.arch.persistence.db.SupportSQLiteOpenHelper;
+import android.arch.persistence.room.DatabaseConfiguration;
+import android.arch.persistence.room.InvalidationTracker;
+import android.content.Context;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import static org.junit.Assert.*;
+
+import edu.gatech.cs2340.hkskh.Database.AppDatabase;
+import edu.gatech.cs2340.hkskh.Shelters.DAOs.ShelterDao;
+import edu.gatech.cs2340.hkskh.Users.DAOs.UserDao;
+import edu.gatech.cs2340.hkskh.Users.Enums.UserType;
+import edu.gatech.cs2340.hkskh.Users.Models.User;
+import edu.gatech.cs2340.hkskh.Users.UserManager;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by Kirby on 4/6/2018.
+ */
+
+@RunWith(MockitoJUnitRunner.class)
+public class RegisterUserTest {
+
+    @Mock
+    private AppDatabase mockedDB;
+
+    private UserManager userManager;
+
+    @Before
+    public void initializeUM() {
+        userManager = new UserManager(this.mockedDB);
+    }
+
+    @Before
+    public void initMocks() {
+        this.mockedDB = mock(AppDatabase.class, Mockito.RETURNS_DEEP_STUBS);
+    }
+
+    @Test
+    public void testRegisterNullFails() {
+        assertEquals(false, userManager.register(null, "Kirby", UserType.USER, "pass"));
+    }
+
+    @Test
+    public void testSmallUsername() {
+        assertEquals(false, userManager.register("ka", "Kirby", UserType.USER, "pass"));
+    }
+
+    @Test
+    public void testUserExists() {
+        when(this.mockedDB.userDao().findUserByUsername("kaz")).thenReturn(getDefaultUser());
+        assertEquals(false, userManager.register("kaz", "Kirby", UserType.USER, "pass"));
+    }
+
+    @Test
+    public void testRegistrationSuccess() {
+        when(this.mockedDB.userDao().findUserByUsername("pete")).thenReturn(null);
+        assertEquals(true, userManager.register("pete", "Pete", UserType.USER, "pass"));
+    }
+
+
+    private User getDefaultUser(){
+        return new User("kaz", UserType.USER, "pass");
+    }
+}
