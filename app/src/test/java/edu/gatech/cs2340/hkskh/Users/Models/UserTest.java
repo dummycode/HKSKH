c621068ebf486b3b21e611291f15f0733867c904
hbao411 <baohyattd@gmail.com>
2018-04-08 19:56:33 -0400

    "Adjusted my junit test"

--- app/src/test/java/edu/gatech/cs2340/hkskh/Users/Models/UserTest.java
+++ app/src/test/java/edu/gatech/cs2340/hkskh/Users/Models/UserTest.java
@@ -9,16 +9,33 @@ import static org.junit.Assert.*;
 
 /**
  * Created by baohd on 4/2/2018.
+ * tests user
  */
 public class UserTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testIllegalArgumentException() throws Exception {
+        BedType bedFam = BedType.FAMILY;
+        BedType bedInd = BedType.INDIVIDUAL;
+        User homeless1 = new User("hbao31", UserType.USER, "123");
+        //This should test that it updates the individual beds
+        homeless1.updateBeds(bedInd, -12);
+    }
     @Test
-    public void updateBeds() throws Exception {
+    public void testIndividualUpdate() throws Exception {
         BedType bedFam = BedType.FAMILY;
         BedType bedInd = BedType.INDIVIDUAL;
         User homeless1 = new User("hbao31", UserType.USER, "123");
         //This should test that it updates the individual beds
         homeless1.updateBeds(bedInd, 11);
         assertEquals(11, homeless1.getNumBeds(bedInd));
+    }
+
+    @Test
+    public void testFamilyUpdate() throws Exception {
+        BedType bedFam = BedType.FAMILY;
+        BedType bedInd = BedType.INDIVIDUAL;
+        User homeless1 = new User("hbao31", UserType.USER, "123");
         //checks family beds are updated
         homeless1.updateBeds(bedFam, 6);
         assertEquals(6, homeless1.getNumBeds(bedFam));
