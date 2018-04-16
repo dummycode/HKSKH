2d56e0fa45e1933d0521f0074362ee138105c887
sporter34 <35616292+sporter34@users.noreply.github.com>
2018-04-16 15:34:09 -0400

    Add files via upload
    added updateVacancy junit test

---
+++ app/src/test/java/edu/gatech/cs2340/hkskh/ShelterManagerTest.java
@@ -0,0 +1,52 @@
+package edu.gatech.cs2340.hkskh;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import edu.gatech.cs2340.hkskh.Database.AppDatabase;
+import edu.gatech.cs2340.hkskh.Shelters.Enums.BedType;
+import edu.gatech.cs2340.hkskh.Shelters.Models.Shelter;
+import edu.gatech.cs2340.hkskh.Shelters.ShelterManager;
+
+
+import static org.mockito.Mockito.mock;
+import static org.junit.Assert.*;
+/**
+ * Created by Steven on 4/16/2018.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ShelterManagerTest {
+    private AppDatabase mockedDb;
+
+    private ShelterManager shelterManager;
+
+    @Before
+    public void initializeUM() {
+        shelterManager = new ShelterManager(this.mockedDb);
+    }
+
+    @Before
+    public void initMocks() {
+        this.mockedDb = mock(AppDatabase.class, Mockito.RETURNS_DEEP_STUBS);
+    }
+
+    @Test
+    public void testUpdateNullVacancy(){
+        shelterManager.updateVacancy(null, BedType.INDIVIDUAL, 1);
+    }
+    @Test
+    public void testUpdateIndividualVacancy(){
+        Shelter shelter = new Shelter("name", 10, 20, "Male", 10.50, 20.50, "555 Fake Street", "Notes", "(555)555-5555");
+        shelterManager.updateVacancy(shelter, BedType.INDIVIDUAL, -5);
+        assertEquals(5, shelter.getVacancyInd());
+    }
+    @Test
+    public void testUpdateFamilyVacancy() {
+        Shelter shelter = new Shelter("name", 10, 20, "Male", 10.50, 20.50, "555 Fake Street", "Notes", "(555)555-5555");
+        shelterManager.updateVacancy(shelter, BedType.FAMILY, -5);
+        assertEquals(15, shelter.getVacancyFam());
+    }
+}
